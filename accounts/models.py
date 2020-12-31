bbe0c32791a8c838d2be0d0df7106fab1796c4b4
Perry Bates <perryboateng08@gmail.com>
2020-12-31 16:39:32 +0000

    fourth commit

--- accounts/models.py
+++ accounts/models.py
@@ -12,6 +12,13 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
 
     CATEGORY = (
@@ -23,8 +30,13 @@ class Product(models.Model):
     name= models.CharField(max_length=200, null=True)
     price= models.FloatField(null=True)
     category= models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description= models.CharField(max_length=200, null=True)
+    description= models.CharField(max_length=200, null=True, blank= True)
     date_created= models.DateField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
 
     
 class Order(models.Model):
@@ -35,7 +47,10 @@ class Order(models.Model):
         ('Delivery', 'Delivery')
     )
 
-   # Customer= models.CharField(max_length=200, null=True)
-    #product= models.FloatField(null=True)
+    customer= models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
+    product= models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
     date_created= models.DateField(auto_now_add=True, null=True)
     status= models.CharField(max_length=200, null=True, choices=STATUS)
+
+
+    
